28482f3dcc376fed82257b2c332ae1a7a1e0ec00
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-05-29 14:39:33 +0700

    feat:add migration post, comment, like

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
--- database/migrations/2024_05_27_072901_create_followers_table.php
+++ database/migrations/2024_05_27_072901_create_followers_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
 
             $table->unique(['follower_id', 'following_id']);
         });
+         
     }
 
     /**
