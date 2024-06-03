14843026d0f0f3ada9eb72bb00887f7629f3b75b
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-06-03 15:40:03 +0700

    feat: add comment and like

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Like;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -16,6 +18,7 @@ class PostController extends Controller
         // $data = $request->all();
 
         $data['user_id'] = $request->user()->id;
+
         $validator = Validator::make($data, [
             'user_id' => 'required',
             'title' => 'required',
@@ -34,12 +37,12 @@ class PostController extends Controller
 
         return response()->json(['message' => 'Post created', 'data' => $data]);
     }
-
+    
     public function getAllPost() {
         $data = Post::get();
         return response()->json(['message' => 'Get post successed', 'data' => $data]);
     }
-
+    
     public function getOnePost($id) {
         if(!is_numeric($id)){
             return response()->json(['message' => 'Please input a valid numeric value'],400);
@@ -48,34 +51,78 @@ class PostController extends Controller
         if(!$post) return response()->json(['message' => 'Post not found'],404);
         return response()->json(['message' => 'Post detail', 'data' => $post]);
     }
-
+    
     public function updatePost($id) {
-
+        
     }
-
+    
     public function deletePost($id) {
 
     }
-
-    public function addLike() {
-
+    
+    public function addLike($id, Request $request) {
+        $data = $request->all();
+        $data['post_id'] = $id;
+    
+        $data['user_id'] = $request->user()->id;
+    
+        $validator = Validator::make($data, [
+            'user_id' => 'required',
+            'post_id' => 'required',
+        ]);
+        
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+    
+        $create = Like::create($data);
+        
+        return response()->json(['message' => 'Like created', 'data' => $data]);
+        
     }
-
+    
     public function deleteLike($id) {
-
+        
     }
+    
+    public function addComment($id, Request $request) {
+        $data = $request->all();
+        
 
-    public function addComment() {
-
+        $data['post_id'] = $id;
+        $data['user_id'] = $request->user()->id;
+        
+        $validator = Validator::make($data, [
+            'user_id' => 'required',
+            'post_id' => 'required',
+            'content' => 'required',
+        ]);
+        
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        
+        $create = Comment::create($data);
+        
+        return response()->json(['message' => 'Comment created', 'data' => $data]);
+        
     }
-
+    
     public function deleteComment($id) {
-
+        
     }
-
+    
     public function updateComment($id) {
-
+        
     }
-
+    
     
 }
